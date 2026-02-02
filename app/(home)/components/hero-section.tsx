1f4cfd3daff39454bd6b3a3068d716d5800d4ec3
RoyDoesCode <roybar03@gmail.com>
2026-02-02 21:01:56 +0200

    Update headline

--- app/(home)/components/hero-section.tsx
+++ app/(home)/components/hero-section.tsx
@@ -8,13 +8,15 @@ import { CLIENTS } from "@/utils/consts";
 
 function HeroSection() {
     return (
-        <section id="hero" className="pt-[calc(50vh-300px)] flex justify-center pb-[10vh]">
+        <section id="hero" className="pt-10 sm:pt-[calc(50vh-250px)] flex justify-center">
             <div className="container text-center px-10">
                 <div className="mx-auto flex max-w-screen-lg flex-col gap-6">
-                    <h1 className="text-4xl font-extrabold lg:text-6xl">שירות לקוחות AI לעסקים בישראל</h1>
+                    <h1 className="text-4xl font-extrabold lg:text-6xl">
+                        ליווי AI לעסק שלך: אוטומציה חכמה שחוסכת זמן ומייצרת שקט תעשייתי
+                    </h1>
                     <p className="text-balance font-rubik text-muted-foreground lg:text-lg">
                         נציגי AI טלפוניים, צ'אטבוטים, וואטסאפ, ועוד... אנחנו לא סוכנות טכנולוגית “גדולה”, אלא צוות שמבין
-                        את המציאות של עסקים בישראל. עם ניסיון טכנולוגי צבאי של +5 שנים, נבנה לכם פתרון חכם ופשוט שעובד
+                        את המציאות של עסקים בישראל. עם ניסיון טכנולוגי צבאי של 5+ שנים, נבנה לכם פתרון חכם ופשוט שעובד
                         באמת.
                     </p>
                 </div>
@@ -42,7 +44,7 @@ function HeroSection() {
                                 <Star key={index} className="size-5 fill-yellow-400 text-yellow-400" />
                             ))}
                         </div>
-                        <p className="text-left font-medium text-muted-foreground">+7 שנים ניסיון</p>
+                        <p className="text-left font-medium text-muted-foreground">7+ שנות ניסיון</p>
                     </div>
                 </div>
             </div>
